fe24d0e8659caed333771460a82fcffe9fe5a29b
Ena Fazlic <ena.fazlic@comtradeintegration.onmicrosoft.com>
2023-08-09 15:09:10 +0200

    ask a question about your pdf

--- app.py
+++ app.py
@@ -1,11 +1,12 @@
 import streamlit as st
+import openai
+import spacy
 from dotenv import load_dotenv
 from PyPDF2 import PdfReader
 from langchain.embeddings.openai import OpenAIEmbeddings
-from langchain.text_splitter import CharacterTextSplitter
-from langchain.vectorstores.redis import Redis
 from langchain.llms import OpenAI
-from langchain.chains.question_answering import load_qa_chain
+from langchain.vectorstores.redis import Redis
+from langchain.chains import ConversationalRetrievalChain
 
 load_dotenv()
 with st.sidebar:
@@ -18,28 +19,54 @@ with st.sidebar:
     - [OpenAI](https://platform.openai.com/docs/models) LLM model
                 ''')
 
+def split_text_into_chunks(text, max_chunk_size):
+    nlp = spacy.load("en_core_web_sm")
+    # Tokenizacija teksta na rečenice
+    doc = nlp(text)
+    sentences = [sent.text for sent in doc.sents]
+
+    chunks = []
+    current_chunk = ""
+    for sentence in sentences:
+        if len(current_chunk) + len(sentence) <= max_chunk_size:
+            current_chunk += sentence
+        else:
+            chunks.append(current_chunk.strip())
+            current_chunk = sentence        
+    chunks.append(current_chunk.strip())
+    
+    return chunks
+
+
 def main():
     st.header("Chat with your own PDF")
     pdf = st.file_uploader("Upload your pdf", type="pdf")
-    
-    if pdf is not None:
+
+    if pdf is not None: #kada korisnik uploaduje pdf fajl, tada ga treba procitati
         pdf_reader = PdfReader(pdf)
-        texts = []
+        chunks = []
+
         for page in pdf_reader.pages:
-            chunks = page.extract_text()
-            texts.append(chunks)
-            st.write(texts)
+            text = page.extract_text()
+            page_chunks = split_text_into_chunks(text,150)
+            chunks.extend(page_chunks) 
         
-        embeddings = OpenAIEmbeddings()
-        VectorStore = Redis.from_texts(texts, embeddings, redis_url="redis://localhost:6379")
+        
+        embeddings = OpenAIEmbeddings() #kreiramo embeddings
+        VectorStore = Redis.from_texts(chunks, embeddings, redis_url="redis://localhost:6379")
+        qa = ConversationalRetrievalChain.from_llm(OpenAI(model = "text-davinci-003",temperature=0), VectorStore.as_retriever()) 
+        chat_history = []
+
         query = st.text_input("Ask questions related to your PDF")
-
+        
+        
         if query:
-            results = VectorStore.similarity_search(query=query, k=3)
-            llm = OpenAI()
-            chain = load_qa_chain(llm=llm, chain_type="stuff")
-            response = chain.run(input_documents=results, question=query)
-            st.write(response) 
+            result = qa({"question": query, "chat_history": chat_history})
+            chat_history.append((query, result['answer']))
+            st.write(result)
 
+            
+        
+            
 if __name__ == '__main__':
     main()
